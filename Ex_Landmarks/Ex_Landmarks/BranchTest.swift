1d5e15fec747153eb58a1c729610171808f87c83
jeonmingyun <jun.min.gyun@jjglobal.kr>
2022-06-20 10:13:56 +0900

    테스트 : 브랜치 추가 후 커

---
+++ Ex_Landmarks/Ex_Landmarks/BranchTest.swift
@@ -0,0 +1,12 @@
+//
+//  branchTest.swift
+//  Ex_Landmarks
+//
+//  Created by jjglobal on 2022/06/20.
+//
+
+import Foundation
+
+struct BranchTest: Codable, Hashable {
+    
+}
---
+++ Ex_Landmarks/Ex_Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,31 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view for bridging a UIPageViewController.
+*/
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            // 슬라이더 이미지
+            PageViewController(pages: pages, currentPage: $currentPage)
+            // 슬라이더 인디케이터
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
